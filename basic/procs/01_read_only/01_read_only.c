6ebb006ffd02abee7f5acc5722b7e08cf2bc5e9b
TarunNagarajan <tarun.greenville@gmail.com>
2025-08-10 15:48:55 +0530

    kernel: needs fixing

--- basic/procs/01_read_only/01_read_only.c
+++ basic/procs/01_read_only/01_read_only.c
@@ -23,7 +23,35 @@ static int hello_open(struct inode* inode, struct file* file) {
 	// file is a pointer to the file structure that tracks the state of this specific file descriptor
 	// if the same file was opened multiple times, each instance of that file would have a different -file- structure
 	return single_open(file, hello_show, NULL);
+	// the third argument, where NULL is, is supposed to be used for custom data to hello_show()
 }
+
+// here, fields map to actions which can be performed on the file
+static const struct proc_ops hello_fops = {
+	.proc_open    = hello_open,
+	.proc_read    = seq_read,
+	.proc_lseek   = seq_lseek,
+	.proc_release = single_release,
+};
+
+static int __init proc_init(void) {
+	proc_create(PROC_NAME, 0, NULL, &hello_fops);
+	// 0 -> file permissions (default)
+	// NULL -> parent directory (I want it to be in the proc root dir)
+	pr_info("hello_proc: /proc/%s\n", PROC_NAME);
+	return 0;
+}
+
+static void __exit proc_exit(void) {
+	remove_proc_entry(PROC_NAME, NULL);
+	pr_info("hello_proc: removed /proc/%s\n", PROC_NAME);
+}
+
+module_init(proc_init);
+module_exit(proc_exit);
+
+
+
 
 
 
