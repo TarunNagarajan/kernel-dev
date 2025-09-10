9ba5a53db187e8c09369b623e2b3d727cef75625
TarunNagarajan <tarun.greenville@gmail.com>
2025-09-10 11:44:26 +0000

    process-mgmt: rough implementation of process monitor, incomplete

--- basic/process-mgmt/state-monitor/state-monitor.c
+++ basic/process-mgmt/state-monitor/state-monitor.c
@@ -30,3 +30,51 @@ static const char* get_task_state_name(long state) {
 	}
 }
 
+static int proc_show(struct seq_file* m, void* v) {
+	struct task_struct* task;
+
+	seq_printf(m, "PID\tCOMM\t\tSTATE\t\tPPID\t\tPRIO\n");
+	seq_printf(m, "---\t----\t\tSTATE\t\tPPID\t\tPRIO\n");
+
+	rcu_read_lock();
+
+	for_each_process(task) {
+		seq_printf(m, "%d\t%-15s\t%-10s\t%d\t%d\n", task->pid, task->comm, get_task_state_name(task->state), task->real_parent->pid, task->prio);
+	}
+
+	rcu_read_unlock();
+
+	return 0;
+}
+
+static int proc_open(struct inode* inode, struct file* file) {
+	return single_open(file, proc_show, NULL);
+}
+
+static const struct proc_ops proc_fops = {
+	.proc_open = proc_open,
+	.proc_read = seq_read,
+	.proc_lseek = seq_lseek,
+	.proc_release = single_release,
+};
+
+static int __init monitor_init(void) {
+	printk(KERN_INFO "Loading Process State Monitor...\n");
+	proc_entry = proc_create("process-monitor", 0444, NULL, &proc_fops);
+
+	if (!proc_entry) {
+		printk(KERN_ERR "Failed to create proc entry.\n");
+		return -ENOMEM;
+	}
+
+	printk(KERN_INFO "Module loaded. Check /proc/process-monitor\n");
+	return 0;
+}
+
+static void __exit monitor_exit(void) {
+	proc_remove(proc_entry);
+	printk(KERN_INFO "Unloaded Process State Monitor...\n");
+}
+
+module_init(monitor_init);
+module_exit(monitor_exit);
