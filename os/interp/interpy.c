84da6babd9565d82c4b365dccc98a42316c5da9a
TarunNagarajan <tarun.greenville@gmail.com>
2025-12-26 07:12:29 +0000

    feat: process creation, heredocs, interactive pipes, file redirection

--- os/interp/interpy.c
+++ os/interp/interpy.c
@@ -9,6 +9,7 @@
 #define MAX_INPUT 1024
 #define MAX_TOKENS 128
 #define MAX_ARGS 64
+#define MAX_COMMANDS 16
 
 typedef struct {
   int append_mode;
@@ -19,6 +20,11 @@ typedef struct {
   char* error_file;
   char* heredoc_delim;
 } Command;
+
+typedef struct {
+  Command commands[MAX_COMMANDS];
+  int command_count;
+} Pipeline;
 
 void prompt() {
   // printf("[interpy] ");
@@ -34,7 +40,7 @@ void prompt() {
   fflush(stdout);
 }
 
-void parse(char* line, Command* cmd) {
+void parse_command(char* line, Command* cmd) {
   char* tokens[MAX_TOKENS];
   int token_count = 0;
   int arg_count = 0;
@@ -106,6 +112,23 @@ void parse(char* line, Command* cmd) {
   cmd->args[arg_count] = NULL;
 }
 
+void parse_pipeline(char* line, Pipeline* pipe) {
+  char* saveptr;
+  char* segment;
+  int count = 0;
+
+  pipe->command_count = 0;
+  segment = strtok_r(line, "|", &saveptr);
+
+  while (segment != NULL && count < MAX_COMMANDS) {
+    parse_command(segment, &pipe->commands[count]);
+    count++;
+    segment = strtok_r(NULL, "|", &saveptr);
+  }
+
+  pipe->command_count = count;
+}
+
 void handle_heredoc(Command* cmd) {
   if (cmd->heredoc_delim == NULL) return;
 
@@ -138,7 +161,58 @@ void handle_heredoc(Command* cmd) {
   cmd->input_file = ".tempheredoc";
 }
 
-int execute(Command* cmd) {
+void launch_process(Command* cmd) {
+  handle_heredoc(cmd); // files override pipes
+  
+  if (cmd->input_file != NULL) {
+    int fd_in = open(cmd->input_file, O_RDONLY);
+    if (fd_in < 0) {
+      perror("input redirection"); 
+      exit(1);
+    }
+    dup2(fd_in, STDIN_FILENO);
+    close(fd_in);
+  }
+
+  if (cmd->output_file != NULL) {
+    int fd_out;
+    if (cmd->append_mode) {
+      fd_out = open(cmd->output_file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    } else {
+      fd_out = open(cmd->output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    }
+
+    if (fd_out < 0) {
+      perror("output redirection");
+      exit(1);
+    }
+
+    dup2(fd_out, STDOUT_FILENO);
+    close(fd_out); 
+  }
+
+  if (cmd->error_file != NULL) {
+    int fd_err = open(cmd->error_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    if (fd_err < 0) {
+      perror("error redirection");
+      exit(1);
+    }
+
+    dup2(fd_err, STDERR_FILENO);
+    close(fd_err);
+  }
+
+  if (cmd->merge_stderr) {
+    dup2(STDOUT_FILENO, STDERR_FILENO);
+  }
+
+  execvp(cmd->args[0], cmd->args);
+  perror("execvp");
+  exit(1);
+}
+
+int execute_command(Command* cmd) {
   handle_heredoc(cmd);
   if (cmd->args[0] == NULL) return 1;
   if (strcmp(cmd->args[0], "exit") == 0) {
@@ -237,9 +311,83 @@ int execute(Command* cmd) {
   return 1;
 }
 
+int execute_pipeline(Pipeline* pipeline) {
+  int i;
+  int prev_fd = -1;
+  int fd[2]; 
+  pid_t pid;
+
+  if (pipeline->command_count == 1) {
+    Command* cmd = &pipeline->commands[0];
+    if (strcmp(cmd->args[0], "exit") == 0) {
+      exit(0);
+    } 
+
+    if (strcmp(cmd->args[0], "cd") == 0) {
+      if (cmd->args[1] == NULL) {
+        fprintf(stderr, "[cd] missing argument\n");
+      } else if (chdir(cmd->args[1]) != 0) {
+        perror("cd"); 
+        return 1;
+      }
+      return 1;
+    }
+  }
+
+  for (i = 0; i < pipeline->command_count; i++) {
+    // DON'T CREATE A PIPE FOR THE LAST COMMAND
+    if (i < pipeline->command_count - 1) {
+      if (pipe(fd) < 0) {
+        perror("pipe");
+        return 1;
+      }
+    }
+
+    pid = fork();
+
+    if (pid < 0) {
+      perror("fork");
+      return 1;
+    }
+
+    if (pid == 0) {
+      if (prev_fd != -1) {
+        dup2(prev_fd, STDIN_FILENO); 
+        close(prev_fd);
+      }
+
+      if (i < pipeline->command_count - 1) {
+        dup2(fd[1], STDOUT_FILENO); 
+        close(fd[1]);
+        close(fd[0]);
+      }
+
+      launch_process(&pipeline->commands[i]);
+    }
+
+    else {
+      if (prev_fd != -1) {
+        close(prev_fd);
+      }
+
+      if (i < pipeline->command_count - 1) {
+        close(fd[1]);
+        prev_fd = fd[0];
+      }
+    }
+  }
+
+  for (int i = 0; i < pipeline->command_count; i++) {
+    wait(NULL);
+  }
+
+  return 1;
+}
+
 int main() {
-  char input[MAX_INPUT]; 
-  Command cmd;
+  char input[MAX_INPUT];
+  Pipeline pipe;
+  // Command cmd;
 
   while (1) {
     prompt();
@@ -255,8 +403,8 @@ int main() {
       continue;
     }
 
-    parse(input, &cmd);
-    execute(&cmd); 
+    parse_pipeline(input, &pipe);
+    execute_pipeline(&pipe); 
   }
 
   return 0;
